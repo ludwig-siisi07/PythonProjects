1722ae5b30a7e388463843fa541b47453fa91fc6
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2025-12-23 21:43:16 +0000

    BMI Calculator

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,7 @@
-bmi = 84 / 1.65 ** 2
+#BMI CALCULATOR
+print("Welcome to the BMI Calculator!")
+#USER INPUTS
+weight = float(input("What is your weight ? "))
+height = float(input("What is your height ? "))
+bmi = round((weight / (height**2)), 2)
+print(f"Your BMI is: {bmi:.2f}kg/m^2")
