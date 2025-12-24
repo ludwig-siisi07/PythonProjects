95c8985733ca350e3fa8aecbeb7648fd43cd8add
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2025-12-24 01:22:49 +0000

    conditionals

--- Day 3/If Else/task.py
+++ Day 3/If Else/task.py
@@ -1,2 +1,6 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("SORRY!\nYou can't ride the rollercoaster!")
