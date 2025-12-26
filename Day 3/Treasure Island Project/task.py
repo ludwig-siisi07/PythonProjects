4d0bb2bc9e4b0a2846980388b9c8601719feaade
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2025-12-26 05:13:55 +0000

    Game

--- Day 3/Treasure Island Project/task.py
+++ Day 3/Treasure Island Project/task.py
@@ -22,3 +22,32 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
+#GAMEPLAYER'S INPUTS AND OUTCOMES
+choice1 = input("You've reached a fork in the path\nChoose a direction:\n"
+                "1. Go left\n2. Go right\nEnter 1 or 2: ")
+if choice1 == "1":
+    action = input("A river blocks your path.\nWhat will you do?\n"
+                   "1. Swim across\n"
+                   "2. Wait for help\n"
+                   "Enter 1 or 2: ")
+    if action == "2":
+        door = input("You find three doors.\nEach door has a different color.\nChoose wisely:\n"
+    "1. Red door\n"
+    "2. Blue door\n"
+    "3. Yellow door\n"
+    "Enter 1, 2, or 3: ")
+        if door == "1":
+            print("Burned by Fire.\n GAME OVER!")
+        elif door == "2":
+            print("Eaten by beasts.\n GAME OVER!")
+        elif door == "3":
+            print("Congratulations!\nYOU HAVE FOUND THE TREASURE!")
+            exit()
+        else:
+            print("GAME OVER!")
+    else:
+        print("Attacked by the trout.\nGAME OVER")
+else:
+    print("You fell into a hole.\nGAME OVER!")
+
+
