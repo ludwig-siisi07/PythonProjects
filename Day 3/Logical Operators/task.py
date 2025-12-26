282536309e06aada2eb9647deff969151a9fa199
ludwig-siisi07 <ludwigsiisibenacquaah@gmail.com>
2025-12-26 04:27:14 +0000

    logical operators

--- Day 3/Logical Operators/task.py
+++ Day 3/Logical Operators/task.py
@@ -6,13 +6,18 @@ if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
     if age < 12:
-        bill = 5
+        bill += 5
         print("Child tickets are $5.")
     elif age <= 18:
-        bill = 7
+        bill += 7
         print("Youth tickets are $7.")
+
+    elif age >= 45 and age <= 55:
+        bill += 0
+        print("You get Free Tickets")
+
     else:
-        bill = 12
+        bill += 12
         print("Adult tickets are $12.")
 
     wants_photo = input("Do you want a photo taken? Y or N. ")
